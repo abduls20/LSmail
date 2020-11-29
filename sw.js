a66806e72bcb30a5b76e3c855cf5dd7851692fd1
abduls20 <68663711+abduls20@users.noreply.github.com>
2020-11-29 14:17:31 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+var cacheName = 'LSmail-pwa';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/main2.js',
+  '/mailgif.gif'
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
